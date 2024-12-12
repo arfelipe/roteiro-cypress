b984236a6efb27862d209857cfdd45087e90e5d5
Arthur Felipe Ferreira <73173371+arfelipe@users.noreply.github.com>
2024-12-11 23:04:40 -0300

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,156 +1,75 @@
 describe('TODOMvc App', () => {
-  
+  const visitApp = () => cy.visit('');
+  const addTodo = (text) => {
+    cy.get('[data-cy=todo-input]').type(`${text}{enter}`);
+  };
+  const getTodosList = () => cy.get('[data-cy=todos-list]').children();
+  const toggleTodo = (index) => {
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]').eq(index).click();
+  };
+  const removeTodo = (index) => {
+    cy.get('[data-cy=todos-list] > li [data-cy=remove-todo-btn]').eq(index).invoke('show').click();
+  };
+
   it('Verifica se app está abrindo', () => {
-    cy.visit('');
+    visitApp();
   });
 
   it('Insere uma tarefa', () => {
-    cy.visit('');
-
-    cy.get('[data-cy=todo-input]')
-      .type('TP2 de Engenharia de Software{enter}');
-
-    cy.get('[data-cy=todos-list]')
-      .children()
-      .should('have.length', 1)
-      .first()
-      .should('have.text', 'TP2 de Engenharia de Software');
+    visitApp();
+    addTodo('TP2 de Engenharia de Software');
+    getTodosList().should('have.length', 1).first().should('have.text', 'TP2 de Engenharia de Software');
   });
 
   it('Insere e deleta uma tarefa', () => {
-    cy.visit('');
-
-    cy.get('[data-cy=todo-input]')
-      .type('TP2 de Engenharia de Software{enter}');
-
-    cy.get('[data-cy=todos-list]')
-      .children()
-      .should('have.length', 1);
-
-    cy.get('[data-cy=todos-list] > li [data-cy=remove-todo-btn]')
-      .invoke('show')
-      .click();
-
-    cy.get('[data-cy=todos-list]')
-      .children()
-      .should('have.length', 0);
+    visitApp();
+    addTodo('TP2 de Engenharia de Software');
+    getTodosList().should('have.length', 1);
+    removeTodo(0);
+    getTodosList().should('have.length', 0);
   });
 
   it('Filtra tarefas completas e ativas', () => {
-    cy.visit('');
+    visitApp();
+    addTodo('TP2 de ES');
+    addTodo('Prova de ES');
+    toggleTodo(0);
 
-    cy.get('[data-cy=todo-input]')
-      .type('TP2 de ES{enter}')
-      .type('Prova de ES{enter}');
+    cy.get('[data-cy=filter-active-link]').click();
+    getTodosList().should('have.length', 1).first().should('have.text', 'Prova de ES');
 
-    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
-      .first()
-      .click();
+    cy.get('[data-cy=filter-completed-link]').click();
+    getTodosList().should('have.length', 1).first().should('have.text', 'TP2 de ES');
 
-    // Filtra as tarefas ativas (não concluídas)
-    cy.get('[data-cy=filter-active-link]')
-      .click();
-    cy.get('[data-cy=todos-list]')
-      .children()
-      .should('have.length', 1)
-      .first()
-      .should('have.text', 'Prova de ES'); // Tarefa ativa
-
-    // Filtra as tarefas concluídas
-    cy.get('[data-cy=filter-completed-link]')
-      .click();
-    cy.get('[data-cy=todos-list]')
-      .children()
-      .should('have.length', 1)
-      .first()
-      .should('have.text', 'TP2 de ES'); // Tarefa concluída
-
-    // Filtra todas as tarefas
-    cy.get('[data-cy=filter-all-link]')
-      .click();
-    cy.get('[data-cy=todos-list]')
-      .children()
-      .should('have.length', 2);
+    cy.get('[data-cy=filter-all-link]').click();
+    getTodosList().should('have.length', 2);
   });
 
   it('Adiciona uma tarefa e marca como concluída', () => {
-    cy.visit('');
-
-    // Adiciona uma tarefa
-    cy.get('[data-cy=todo-input]')
-      .type('Fazer compras{enter}');
-
-    // Verifica se a tarefa foi adicionada
-    cy.get('[data-cy=todos-list]')
-      .children()
-      .should('have.length', 1)
-      .first()
-      .should('have.text', 'Fazer compras');
-
-    // Marca a tarefa como concluída
-    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
-      .click();
-
-    // Verifica se a tarefa está marcada como concluída
-    cy.get('[data-cy=todos-list] > li')
-      .should('have.class', 'completed');
+    visitApp();
+    addTodo('Fazer compras');
+    getTodosList().should('have.length', 1).first().should('have.text', 'Fazer compras');
+    toggleTodo(0);
+    cy.get('[data-cy=todos-list] > li').should('have.class', 'completed');
   });
 
   it('Excluir tarefa após marcar como concluída', () => {
-    cy.visit(''); // Insira o URL ou caminho correto aqui
-
-    // Adiciona uma tarefa
-    cy.get('[data-cy=todo-input]')
-      .type('Estudar para a prova{enter}');
-
-    // Marca a tarefa como concluída
-    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
-      .click();
-
-    // Verifica se a tarefa está concluída
-    cy.get('[data-cy=todos-list] > li')
-      .should('have.class', 'completed');
-
-    // Remove a tarefa
-    cy.get('[data-cy=todos-list] > li [data-cy=remove-todo-btn]')
-      .invoke('show')
-      .click();
-
-    // Verifica se a lista está vazia
-    cy.get('[data-cy=todos-list]')
-      .children()
-      .should('have.length', 0);
+    visitApp();
+    addTodo('Estudar para a prova');
+    toggleTodo(0);
+    cy.get('[data-cy=todos-list] > li').should('have.class', 'completed');
+    removeTodo(0);
+    getTodosList().should('have.length', 0);
   });
 
   it('Excluir uma tarefa incompleta', () => {
-    cy.visit('');
-
-    // Adiciona várias tarefas
-    cy.get('[data-cy=todo-input]')
-      .type('Fazer compras{enter}')
-      .type('Estudar para a prova{enter}')
-      .type('Ler um livro{enter}');
-
-    // Marca a segunda tarefa como concluída
-    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
-      .eq(1)
-      .click();
-
-    // Verifica se a segunda tarefa foi marcada como concluída
-    cy.get('[data-cy=todos-list] > li')
-      .eq(1)
-      .should('have.class', 'completed');
-
-    // Exclui a primeira tarefa (incompleta)
-    cy.get('[data-cy=todos-list] > li [data-cy=remove-todo-btn]')
-      .eq(0)  // Seleciona a tarefa incompleta
-      .invoke('show')
-      .click();
-
-    // Verifica se a primeira tarefa foi excluída
-    cy.get('[data-cy=todos-list]')
-      .children()
-      .should('have.length', 2); // Deve restar duas tarefas
+    visitApp();
+    addTodo('Fazer compras');
+    addTodo('Estudar para a prova');
+    addTodo('Ler um livro');
+    toggleTodo(1);
+    cy.get('[data-cy=todos-list] > li').eq(1).should('have.class', 'completed');
+    removeTodo(0);
+    getTodosList().should('have.length', 2);
   });
-
 });
